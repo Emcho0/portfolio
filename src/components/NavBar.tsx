7842250831a762f4972706f8a23ae6f1be0780d3
Emcho0 <emir.kusundzija@gmail.com>
2025-06-09 18:04:13 +0200

    fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <nav className="text-2xl font-mono bg-blue-500 p-4 space-x-3">
       <Link to="/">Home</Link>
-      <Link to="/testing">Testing</Link>
+      <Link to="/test">Testing</Link>
     </nav>
   );
 };
